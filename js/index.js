a0e84e128ccd3bb750ac0fee3814443042f144a1
rahul3411 <jkjp9835@gmail.com>
2023-02-18 16:47:10 +0530

    first commit

---
+++ js/index.js
@@ -0,0 +1,7 @@
+// ##### typing animation #######
+var typed = new Typed(".typing",{
+strings:["","Web Desginer" , "Web Developer" , "Graphic Designer","Logo Maker"],
+typeSpeed:100,
+BackSpeed:60,
+loop:true
+})
